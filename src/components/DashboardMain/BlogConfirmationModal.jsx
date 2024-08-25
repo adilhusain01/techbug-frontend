cc26d7dbd81921ee1761c6179a6af8608d3d157d
adilhusain01 <husainadil202@gmail.com>
2024-08-25 14:24:35 +0530

    renamed confirmaationModal to blogConfirmationModal

--- src/components/DashboardMain/BlogConfirmationModal.jsx
+++ src/components/DashboardMain/BlogConfirmationModal.jsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-function ConfirmationModal({
+function BlogConfirmationModal({
   open,
   handleClose,
   handleDelete,
@@ -75,4 +75,4 @@ function ConfirmationModal({
   );
 }
 
-export default ConfirmationModal;
+export default BlogConfirmationModal;
